3ad6c5a53384b99c419843e81c44eb027ac62206
AhmedSaad <krarooo281@gmail.com>
2018-10-20 21:37:40 +0200

    Update FadingTextView.java

--- fadingtextview/src/main/java/com/tomer/fadingtextview/FadingTextView.java
+++ fadingtextview/src/main/java/com/tomer/fadingtextview/FadingTextView.java
@@ -40,6 +40,44 @@ public class FadingTextView extends android.support.v7.widget.AppCompatTextView 
 	
 	public FadingTextView(Context context, AttributeSet attrs) {
 		super(context, attrs);
+		 if(context.getString(R.string.lang).equals("ar")) {
+            setTypeface(FontManger.yad);
+        }if (context.getString(R.string.lang).equals("zh")) {
+            setTypeface(FontManger.english);
+        }if (context.getString(R.string.lang).equals("cs")){
+            setTypeface(FontManger.english);
+        }if (context.getString(R.string.lang).equals("nl")){
+            setTypeface(FontManger.english);
+        }if (context.getString(R.string.lang).equals("en")){
+            setTypeface(FontManger.english);
+        } if(context.getString(R.string.lang).equals("fr")) {
+            setTypeface(FontManger.english);
+        }if (context.getString(R.string.lang).equals("de")) {
+            setTypeface(FontManger.english);
+        }if (context.getString(R.string.lang).equals("in")){
+            setTypeface(FontManger.english);
+        }if (context.getString(R.string.lang).equals("it")){
+            setTypeface(FontManger.english);
+        }if (context.getString(R.string.lang).equals("ja")){
+            setTypeface(FontManger.english);
+        }if (context.getString(R.string.lang).equals("ko")){
+            setTypeface(FontManger.english);
+        }if (context.getString(R.string.lang).equals("fa")){
+            setTypeface(FontManger.yad);
+        }if (context.getString(R.string.lang).equals("pl")){
+            setTypeface(FontManger.english);
+        } if(context.getString(R.string.lang).equals("pt")) {
+            setTypeface(FontManger.english);
+        }if (context.getString(R.string.lang).equals("ro")) {
+            setTypeface(FontManger.english);
+        }if (context.getString(R.string.lang).equals("ru")){
+            setTypeface(FontManger.english);
+        }if (context.getString(R.string.lang).equals("es")){
+            setTypeface(FontManger.english);
+        }if (context.getString(R.string.lang).equals("th")){
+            setTypeface(FontManger.english);
+        }else if (context.getString(R.string.lang).equals("tr"))
+            setTypeface(FontManger.english);
 		init();
 		handleAttrs(attrs);
 	}
